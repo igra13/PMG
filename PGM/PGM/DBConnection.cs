bc2410265bcbf8ee32e6635332120eca9ea1ed83
igra13 <igorradulovic400@gmail.com>
2017-10-10 10:40:08 +0200

    Initial commit.

---
+++ PGM/PGM/DBConnection.cs
@@ -0,0 +1,79 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Data
+{
+    public class DBConnection
+    {
+        private DBConnection()
+        {
+        }
+
+        private string databaseName = string.Empty;
+        public string DatabaseName
+        {
+            get { return databaseName; }
+            set { databaseName = value; }
+        }
+
+        public string Password { get; set; }
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static DBConnection _instance = null;
+        public static DBConnection Instance()
+        {
+            if (_instance == null)
+                _instance = new DBConnection();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            bool result = true;
+            if (Connection == null)
+            {
+               
+                    result = false;
+                string server = "188.166.43.154";
+                string database = "pmg";
+                string uid = "tina";
+                string password = "tina";
+                string connectionString;
+                connectionString = "SERVER=" + server + "; PORT = 3306 ;" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+
+
+                using (connection = new MySqlConnection(connectionString))
+                {
+                    try
+                    {
+                        connection.Open();
+
+                        MessageBox.Show(" open connection ! ");
+                        result = true;
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Can not open connection ! ");
+                    }
+                }
+                
+            }
+
+            return result;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
---
+++ PGM/PGM/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+namespace PGM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            string server = "188.166.43.154";
+            string database = "pmg";
+            string uid = "tina";
+            string password = "tina";
+            string connectionString;
+            connectionString = "SERVER=" + server + "; PORT = 3306 ;" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+
+            var dbCon = DBConnection.Instance();
+            dbCon.DatabaseName = "pmg";
+            if (dbCon.IsConnect())
+            {
+                MessageBox.Show("CONNECTED");
+                //hämta
+                //string CmdString = "SELECT user_login FROM wp_users";
+                //MySqlDataAdapter sda = new MySqlDataAdapter(CmdString, dbCon.Connection);
+                //DataSet ds = new DataSet();
+                //sda.Fill(ds);
+                
+                //dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+                //lägg till
+               string ao = "INSERT INTO wp_users (ID, user_login, user_pass, user_nicename, user_email, user_url, user_registered, user_activation_key, user_status, display_name) VALUES (NULL, 'tinasda', 'tinasda', 'tinasda', 'igor@hotmail.com', '', '2017-10-12 00:00:00', '', '0', 'tinada')";
+                
+                MySqlDataAdapter sd = new MySqlDataAdapter(ao, dbCon.Connection);
+
+                //MySqlCommand insere = new MySqlCommand();
+
+                //insere.Connection = dbCon.Connection;  //<- Set the Commands connection
+
+                //insere.CommandText = "INSERT INTO wp_users (ID, user_login, user_pass, user_nicename, user_email, user_url, user_registered, user_activation_key, user_status, display_name) VALUES (NULL, 'tina', 'tina', 'tina', 'igor@hotmail.com', '', '2017-10-12 00:00:00', '', '0', 'tina')";
+
+               
+                ////suppose col0 and col1 are defined as VARCHAR in the DB
+                //string query = "SELECT col0,col1 FROM YourTable";
+                //var cmd = new MySqlCommand(query, dbCon.Connection);
+                //var reader = cmd.ExecuteReader();
+                //while (reader.Read())
+                //{
+                //    string someStringFromColumnZero = reader.GetString(0);
+                //    string someStringFromColumnOne = reader.GetString(1);
+                //    Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+                //}
+            }
+            else
+            {
+                MessageBox.Show("NOT CONNECTED");
+            }
+
+
+        }
+    }
+}
